4faec0c82c993c682bca0b3cb1a025e57dd03f4c
Vincent Riva <vincent.riva@outlook.fr>
2021-04-27 12:19:55 +0200

    Begin to add SubAccounts endpoints

--- src/lib/SubAccount.ts
+++ src/lib/SubAccount.ts
@@ -66,3 +66,162 @@ export interface ISubAccountsAssetsBalance {
   free: number;
   locked: number;
 }
+
+export interface ISubAccountsCreateVirtualSubAccountParameters extends Binance.IRequestParameters {
+  subAccountString: string;
+}
+
+export interface ISubAccountsCreateVirtualSubAccount {
+  email: string;
+}
+
+export interface ISubAccountsQuerySubAccountListSAPIParameters extends Binance.IRequestParameters {
+  email?: string;
+  isFreeze?: string;
+  page?: number;
+  limit?: number;
+}
+
+export interface ISubAccountsQuerySubAccountListSAPI {
+  subAccounts: ISubAccountsQuerySubAccountListSAPIItem[];
+}
+
+export interface ISubAccountsQuerySubAccountListSAPIItem {
+  email: string;
+  isFreeze: boolean;
+  createTime: number;
+}
+
+export interface ISubAccountsQuerySubAccountListParameters extends Binance.IRequestParameters {
+  email?: string;
+  status?: string;
+  page?: number;
+  limit?: number;
+}
+
+export interface ISubAccountsQuerySubAccountList {
+  subAccounts: ISubAccountsQuerySubAccountListItem[];
+  success: boolean;
+}
+
+export interface ISubAccountsQuerySubAccountListItem {
+  email: string;
+  status: string;
+  activated: boolean;
+  mobile: string;
+  gAuth: boolean;
+  createTime: number;
+}
+
+export interface ISubAccountsQuerySubAccountSpotAssetTransferHistoryParameters extends Binance.IRequestParameters {
+  fromEmail?: string;
+  toEmail?: string;
+  startTime?: number;
+  endTime?: number;
+  page?: number;
+  limit?: number;
+}
+
+export interface ISubAccountsQuerySubAccountSpotAssetTransferHistory {
+  transfers: ISubAccountsQuerySubAccountSpotAssetTransferHistoryItem[];
+  success: boolean;
+}
+
+export interface ISubAccountsQuerySubAccountSpotAssetTransferHistoryItem {
+  from: string;
+  to: string;
+  asset: string;
+  qty: string;
+  status: string;
+  tranId: number;
+  time: number;
+}
+
+export interface ISubAccountsQuerySubAccountSpotAssetTransferHistorySAPIParameters extends Binance.IRequestParameters {
+  fromEmail?: string;
+  toEmail?: string;
+  startTime?: number;
+  endTime?: number;
+  page?: number;
+  limit?: number;
+}
+
+export type ISubAccountsQuerySubAccountSpotAssetTransferHistorySAPI = ISubAccountsQuerySubAccountSpotAssetTransferHistoryItem;
+
+export interface ISubAccountsSubAccountSpotAssetTransferParameters extends Binance.IRequestParameters {
+  fromEmail: string;
+  toEmail: string;
+  asset: string;
+  amount: number;
+}
+
+export interface ISubAccountsSubAccountSpotAssetTransfer {
+  txnId: string;
+  success: boolean;
+}
+
+export interface ISubAccountsQuerySubAccountFuturesAssetTransferHistoryParameters extends Binance.IRequestParameters {
+  email: string;
+  futuresType: Binance.EFuturesType;
+  startTime?: number;
+  endTime?: number;
+  page?: number;
+  limit?: number;
+}
+
+export interface ISubAccountsQuerySubAccountFuturesAssetTransferHistory {
+  futuresType: Binance.EFuturesType;
+  success: boolean;
+  transfers: ISubAccountsQuerySubAccountFuturesAssetTransferHistoryItem[];
+}
+
+export interface ISubAccountsQuerySubAccountFuturesAssetTransferHistoryItem {
+  from: string;
+  to: string;
+  asset: string;
+  qty: string;
+  tranId: number;
+  time: number;
+}
+
+export interface ISubAccountsSubAccountFuturesAssetTransferParameters extends Binance.IRequestParameters {
+  fromEmail: string;
+  toEmail: string;
+  asset: string;
+  amount: number;
+  futuresType: Binance.EFuturesType;
+}
+
+export interface ISubAccountsSubAccountFuturesAssetTransfer {
+  txnId: string;
+  success: boolean;
+}
+
+export interface ISubAccountsQuerySubAccountAssetsParameters extends Binance.IRequestParameters {
+  email: string;
+}
+
+export interface ISubAccountsQuerySubAccountAssets {
+  success: boolean;
+  balances: ISubAccountsQuerySubAccountAssetsItem[];
+}
+
+export interface ISubAccountsQuerySubAccountAssetsItem {
+  asset: string;
+  free: number;
+  locked: number;
+}
+
+export interface ISubAccountsQuerySubAccountAssetsSAPIParameters extends Binance.IRequestParameters {
+  email: string;
+}
+
+export interface ISubAccountsQuerySubAccountAssetsSAPI {
+  balances: ISubAccountsQuerySubAccountAssetsItem[];
+}
+
+export interface ISubAccountsQuerySubAccountAssetsSAPIItem {
+  asset: string;
+  free: number;
+  locked: number;
+}
